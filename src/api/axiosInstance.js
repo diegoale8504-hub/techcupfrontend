0375a158ea8fcb578a971aea6b79509cf7b15b47
Alejandro Rozo <diego.rozo-g@mail.escuelaing.edu.co>
2026-04-15 11:54:43 -0500

    Se ha arreglado el apartado de perfil

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -7,7 +7,7 @@ const axiosInstance = axios.create({
 })
 
 axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('techcup_token')
+  const token = localStorage.getItem('token')
   if (token) {
     config.headers.Authorization = `Bearer ${token}`
   }
@@ -24,7 +24,7 @@ axiosInstance.interceptors.response.use(
       if (error.response.data?.message) {
         error.userMessage = error.response.data.message
       } else {
-        localStorage.removeItem('techcup_token')
+        localStorage.removeItem('token')
         window.dispatchEvent(new Event('auth:unauthorized'))
       }
     } else if (error.response.status === 403) {
